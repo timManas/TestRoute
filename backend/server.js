fd2e8d2ccea6e3e95d363ff3ec799c8328060758
TimManas <timothy.romero.manas@gmail.com>
2021-02-08 23:29:05 -0500

    Add stuff

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,21 @@ connectDB()
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('API is running....')
-})
+// RUN THIS ONLY IN PRODUCTION MODE
+if (process.env.NODE_ENV === 'production') {
+  // Set the 'build' folder as a static folder
+  // Why ? So we can access the build folder and load the index.html
+  app.use(express.static(path.join(__dirname, '/frontend/build'))) // set frontend folder as a static folder
+
+  // Send the index.html file if on Production
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running ....')
+  })
+}
 
 app.use('/api/products', productRoutes)
 
